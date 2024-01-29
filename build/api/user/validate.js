fd49d75b7c698dfe0930f1dc97749daa33244237
Thiago-dcode <contact.thiago.ferreira@gmail.com>
2024-01-29 22:42:22 +0100

    changes

---
+++ build/api/user/validate.js
@@ -0,0 +1,19 @@
+import { z } from "zod";
+const User = z.object({
+    name: z.string().min(3).optional(),
+    username: z.string().min(5).toLowerCase(),
+    password: z.string().min(8, {
+        message: "Must contain at least 8 character(s)",
+    }),
+    email: z.string().email().optional(),
+    biography: z.string().optional(),
+    avatar: z.string().optional(),
+    isAdmin: z.boolean().default(false).optional(),
+});
+export const validateCreate = (data) => {
+    const result = User.safeParse(data);
+    return result;
+};
+export const validateUpdate = (data) => {
+    return User.optional().safeParse(data);
+};
